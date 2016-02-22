1817e7b718ab1154a1db52f039cc0a7c137f807e
Mikimaro Yonekura <MikimaroYonekura@Mikimaro-no-MacBook-Pro.local>
2016-02-22 21:37:12 +0900

    no message

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 	require_once './header.php';
 ?>
-<div id="main">	
-	<div id="top">
-		<img src="image/top01.png" class="topImg">
+<div>
+	<div id="main">	
+		<div id="top">
+			<img src="image/top01.png" class="topImg">
+		</div>
+	</div>
+	<div id="sub">
+		<ul>
+			<!-- Dojoの日程 -->
+			<li class="subImg"><a href="#"><img src="./image/date.jpg"></a></li>
+			<!-- 最近のニュース -->
+			<li class="subImg"><a href="#"><img src="./image/news.jpg"></a></li>
+			<!-- ポートフォリオ -->
+			<li class="subImg"><a href="#"><img src="./image/portfolio.jpg"></a></li>
+		</ul>
 	</div>
 </div>
-<div id="sub">
-	<ul>
-		<!-- Dojoの日程 -->
-		<li class="sub"><a href="#"><img src="./image/date.jpg"></a></li>
-		<!-- 最近のニュース -->
-		<li><a href="#"><img src="./image/news.jpg"></a></li>
-		<!-- ポートフォリオ -->
-		<li><a href="#"><img src="./image/portfolio.jpg"></a></li>
-	</ul>
-</div>
-
 </body>
 </html>
