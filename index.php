cb5e78862b515429ca2e6a0261d159886afd570e
Mikimaro Yonekura <MikimaroYonekura@Mikimaro-no-MacBook-Pro.local>
2016-02-22 19:56:50 +0900

    no message

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
 		</ul>
 	</div>
 </div>
+<div id="sub">
+	<ul>
+		<!-- Dojoの日程 -->
+		<li class="sub"><a href="#"><img src="./image/date.jpg"></a></li>
+		<!-- 最近のニュース -->
+		<li><a href="#"><img src="./image/news.jpg"></a></li>
+		<!-- ポートフォリオ -->
+		<li><a href="#"><img src="./image/portfolio.jpg"></a></li>
+	</ul>
+</div>
 
 </body>
 </html>
