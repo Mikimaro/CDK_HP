e2bde7c73a74e68d9795715166d4bc591cc41c71
mjk0513 <kirie0513@gmail.com>
2016-02-22 17:34:11 +0900

    とりあえず作った

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="CoderDojo,CoderDojo Kashiwa,こーだーどーじょー,千葉,柏,プログラミング教室,小学生,中学生,プログラミング">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/flexslider.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="./script/jquery.flexslider.js"></script>
+		
+	<script type="text/javascript" charset="utf-8">
+		$(window).load(function() {
+			$('.flexslider').flexslider();
+		});
+	</script>
+
+	<title>CoderDojo Kashiwa</title>
+</head>
+
+<body>
+	<div id="menu">
+		<ul>
+			<a href="./index.php"><li></li></a>
+			<a href="./about.php"><li>CoderDojoとは</li></a>
+			<a href="./curriculum.php"><li>カリキュラム</li></a>
+			<a href="./join.php"><li>参加する</li></a>
+			<a href="./mentor.php"><li>メンター</li></a>			
+			<a href="./event.php"><li>イベント</li></a>
+			<a href="./blog.php"><li>ブログ</li></a>
+		</ul>
+	</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	require_once './header.php';
+?>
+
+
+
+<div id="main">
+	<div class="flexslider">
+		<ul class="slides">
+			<li><img src="./image/slides/001.jpg"></li>
+			<li><img src="./image/slides/002.jpg"></li>
+		</ul>
+	</div>
+</div>
+
+</body>
+</html>
