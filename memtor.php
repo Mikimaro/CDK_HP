9a48501bb291ecdd85cbc7956969add92734de5b
mjk0513 <kirie0513@gmail.com>
2016-03-22 01:33:48 +0900

    各種整形

--- memtor.php
+++ memtor.php
@@ -141,47 +141,6 @@
 		)
 
 	);
-/*	
-	$studentMentorName = array("宮島 衣瑛", "加藤 遼", "鈴木 貴也", "飯島 巧", "牧野 翔", "北原 成貴", "米倉 幹麿", "相良 亮","竹内 遥斗", "村脇 光洋", "福田 敏史", "梅田真妃留", "窪田碧唯", "成澤巧徒");
-	$adultMentorName = array("石原怜", "坂田健一", "間下知紀", "高松基広", "小野寺宣裕", "西村三千代", "小館直人");
-	$studentMentorSkil = array(
-		"Scratch / Swift / HTML, CSS / PHP",
-		"Scratch / HTML, CSS",
-		"Scratch",
-		"Scratch",
-		"Scratch",
-		"映像",
-		"映像 / HTML, CSS",
-		"Scratch",
-		"Scratch",
-		"Scratch / Swift / JavaScript",
-		"Scratch / Java",
-		"Scratch",
-		"Scratch",
-		"Scratch / 電気工作 / Java"
-	);
-
-
-
-	$studentMentorDojo = array(
-		"Kashiwa / Kashiwa-no-ha",
-		"Kashiwa",
-		"Kashiwa / Kashiwa-no-ha",
-		"Kashiwa / Kashiwa-no-ha",
-		"Kashiwa",
-		"Kashiwa",
-		"Kashiwa / Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa-no-ha",
-		"Kashiwa"
-	);
-
-
-*/
 
 	require_once 'header.php';
 
