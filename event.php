8574d204465a9b066cc506cbfc40109e0b4b8f40
Mikimaro Yonekura <MikimaroYonekura@Mikimaro-no-MacBook-Pro.local>
2016-03-22 00:45:17 +0900

    no message

--- event.php
+++ event.php
@@ -12,22 +12,24 @@
 
 			<h1 class="headline">Special Events</h1>
 
-			<div class="past-events">
+		</div>
+
+		<div class="past-events">
 
 				<div class="event-list clearfix"> 
 
 					<?php
-						$img = array('./image/eventLogos/spd2015.png','./image/eventLogos/jc2015.png','./image/eventLogos/spd2014.png');
+						$img = array('dummy','./image/eventLogos/spd2015.png','./image/eventLogos/jc2015.png','./image/eventLogos/spd2014.png');
 
-						$title = array('Special Presentation Day 2015','June Conference 2015','Special Presentation Day 2014');
+						$title = array('dummy event', 'Special Presentation Day 2015','June Conference 2015','Special Presentation Day 2014');
 
-						$day = array('2015.12.20','2015.06.07','2014.12.21');
+						$day = array('dummy day', '2015.12.20','2015.06.07','2014.12.21');
 
-						$site = array('@KOILスタジオ','@NoblesseOblige','NoblesseOblige');
+						$site = array('dummy place', '@KOILスタジオ','@NoblesseOblige','NoblesseOblige');
 
-						$comments = array('イノベーションラボ柏の葉KOILでの作品発表会','CoderDojo Kashiwaで初の技術的な発表会','CoderDojo Kashiwaで初の子供たちのための作品発表会');
+						$comments = array('none comment','イノベーションラボ柏の葉KOILでの作品発表会','CoderDojo Kashiwaで初の技術的な発表会','CoderDojo Kashiwaで初の子供たちのための作品発表会');
 
-						$URL = array('#','#','#');
+						$URL = array('#','#','#','#');
 
 						for ($i=0; $i < count($title); $i++) { 
 							print("<div class='party'>
@@ -43,26 +45,12 @@
 											</div>
 										</div>
 									</article>
-									<article class='mainArticle'>
-										<div>
-											<img src='{$img[++$i]}' class='eventImg'>
-											<div class='eventText'>
-												<h2>{$title[$i]}</h2>
-												<h3>{$day[$i]}</h3>
-												<h4>{$site[$i]}</h4>
-												<h5>{$comments[$i]}</h5>
-												<a href='{$URL[$i]}' class='readMore'>より詳しく</a>
-											</div>
-										</div>
-									</article>
-								</div>");
+									</div>");
 						}
 
 					?>
 					
 				</div>
-				
-			</div>
 			
 		</div>
 
--- memtor.php
+++ memtor.php
@@ -1,7 +1,42 @@
 <?php
 	
-	$studentMentorName = array("宮島 衣瑛", "加藤 遼", "鈴木 貴也", "飯島 巧", "牧野 翔", "北原 成貴", "米倉 幹麿", "相良 亮","竹内 遥斗", "村脇 光洋", "福田 敏史", "まきろに", "あをさん");
+	$studentMentorName = array("宮島 衣瑛", "加藤 遼", "鈴木 貴也", "飯島 巧", "牧野 翔", "北原 成貴", "米倉 幹麿", "相良 亮","竹内 遥斗", "村脇 光洋", "福田 敏史", "梅田真妃留", "窪田碧唯", "成澤巧徒");
 	$adultMentorName = array("石原怜", "坂田健一", "間下知紀", "高松基広", "小野寺宣裕", "西村三千代", "小館直人");
+	$studentMentorSkil = array(
+		"Scratch / Swift / HTML, CSS / PHP",
+		"Scratch / HTML, CSS",
+		"Scratch",
+		"Scratch",
+		"Scratch",
+		"映像",
+		"映像 / HTML, CSS",
+		"Scratch",
+		"Scratch",
+		"Scratch / Swift / JavaScript",
+		"Scratch / Java",
+		"Scratch",
+		"Scratch",
+		"Scratch / 電気工作 / Java"
+
+	);
+
+	$studentMentorDojo = array(
+		"Kashiwa / Kashiwa-no-ha",
+		"Kashiwa",
+		"Kashiwa / Kashiwa-no-ha",
+		"Kashiwa / Kashiwa-no-ha",
+		"Kashiwa",
+		"Kashiwa",
+		"Kashiwa / Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa-no-ha",
+		"Kashiwa"
+	);
+
 
 
 
@@ -12,21 +47,69 @@
 
 <div id="conteiner">
 
-	<p class="text">
-		CoderDojo Kashiwa & Kashiwa-no-haのメンターを紹介します。
-	</p>
+	<div id="mentor">
 
-	<table border="1">
+		<div id="pageTitle">
 
-	<?php
+			<img src="#"  class="headerLogo" alt="">
 
-		for ($i=0; $i < count($studentMentorName); $i+=2) { 
-			print("<tr><td class='tdL'>{$studentMentorName[$i]}</td><td class='tdR'>{$studentMentorName[$i+1]}</td></tr>");
-		}
-	?>
+			<h1 class="headline">メンター紹介</h1>
 
-	</table>
+			<p class="headline">
+				CoderDojo Kashiwa & Kashiwa-no-haのメンターを紹介します。
+			</p>
 
+		</div>
+
+		<div class="mentor-list clearfix">
+
+			<div class="mentorList-L">
+				<ul class="mentorListUL">
+					<?php
+						for ($i=0; $i < count($studentMentorName); $i+=2) { 
+							$imgNum = $i + 1;
+							print("<li class='mentorListLI'>");
+							if ($i == 1) {
+								print("<img src='image/mentorImage/s{$imgNum}.png' class='mentorImg'>");
+							}else{
+								print("<img src='image/mentorImage/s{$imgNum}.jpg' class='mentorImg'>");
+							}
+							print("<div class='montorProfile'>");
+							print("<p class='name'>{$studentMentorName[$i]}</p>");
+							print("<p class='skil'>{$studentMentorSkil[$i]}</p>");
+							print("<p class='belonging'>{$studentMentorDojo[$i]}</p>");
+							print("</div>");
+							print("</li>");
+
+						}
+					?>
+
+				</ul>
+			</div>
+
+			<div class="mentorList-R">
+				<ul class="mentorListUL">
+					<?php
+						for ($i=1; $i < count($studentMentorName); $i+=2) { 
+							$imgNum = $i + 1;
+							print("<li class='mentorListLI'>");
+							if ($i == 1) {
+								print("<img src='image/mentorImage/s{$imgNum}.png' class='mentorImg'>");
+							}else{
+								print("<img src='image/mentorImage/s{$imgNum}.jpg' class='mentorImg'>");
+							}
+							print("</li>");
+							print("<li>{$studentMentorName[$i]}</li>");
+							print("<li>{$studentMentorSkil[$i]}");
+							print("<li>{$studentMentorDojo[$i]}");
+
+						}
+					?>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php
