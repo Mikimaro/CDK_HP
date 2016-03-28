489f6b5cac6b72b4803c809bcb057893f4deae9a
Mikimaro Yonekura <MikimaroYonekura@Mikimaro-no-MacBook-Pro.local>
2016-03-29 01:07:47 +0900

    contactページの完成ですかね？ まあ残りはトップページのもろもろとフッターのリンクですね

--- contact.php
+++ contact.php
@@ -14,11 +14,37 @@
 
 		<div id="contact">
 			
-			<div class="SNS">
+			<div class="contactContents">
+
+				<h4 class="headline">CoderDojo Kashiwaの公式SNSを紹介します。</h4>
+
+				<div class="snsContents">
+
+					<a href="http://twitter.com/c_d_kashiwa"><img src="./image/contact/TwitterLogo.png" class="snsLogo" alt="twitter"></a>
+
+					<a href="http://facebook.com/CoderDojokashiwa"><img src="./image/contact/FBLogo.png" class="snsLogo snsImgAdj"　alt="facebook"></a>
+
+				</div>
 				
 			</div>
 
-			<div class="">
+			<div class="contactContents">
+
+				<h4 class="headline">CoderDojo Kashiwaに関するお問合わせ先を紹介します。</h4>
+
+				<div class="otherContents">
+
+					<img src="./image/contact/mailLogo.png" class="generalImgAdj" >
+
+					<div class="info">
+
+						<a href="mailto:coderdojo-kashiwa@gmail.com" class="mail">coderdojo-kashiwa (at) gmail.com</a>
+
+						<p class="mailAttention">メールアドレスをクリックしていただくか"(at)"の部分を"＠"に変えて送信してください。</p>
+
+					</div>
+
+				</div>
 				
 			</div>
 
--- memtor.php
+++ memtor.php
@@ -1,5 +1,5 @@
 <?php
-	$mentorData = array(
+	$studentMentorData = array(
 		0 => array(
 			"name" => "宮島 衣瑛",
 			"en" => "Miyajima Kirie",
@@ -152,8 +152,132 @@
 			"dojo" => array(
 				"Kashiwa-no-ha"
 			)
+		),
+
+		14 => array(
+			"name" => "大熊 謙輔",
+			"en" => "Ookura Kensuke",
+			"skil" => array(
+				"Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
 		)
 
+	);
+
+	$adultMentorData = array(
+		0 => array(
+			"name" => "石原 怜",
+			"en" => "Ishihara Ryou",
+			"skil" => array(
+				"Scratch", "Objective-C"
+			),
+			"dojo" =>array(
+				"Kashiwa / Kashiwa-no-ha"
+			)
+		),
+
+		1 => array(
+			"name" => "小野寺 宣裕",
+			"en" => "Onodera Nobuhiro",
+			"skil" => array(
+				"Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		2 => array(
+			"name" => "鎌田 泰行",
+			"en" => "Kamata Yasuyuki",
+			"skil" => array(
+				"Scratch","Unity"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		3 => array(
+			"name" => "坂田 健一",
+			"en" => "Sakata Kenchi",
+			"skil" => array(
+				"Scratch","ActionScript"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		4 => array(
+			"name" => "櫻庭 敬之",
+			"en" => "Sakuraba Noriyuki",
+			"skil" => array(
+				"Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		5 => array(
+			"name" => "澤 孝治",
+			"en" => "Sawa Takaharu",
+			"skil" => array(
+				"映像","Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		6 => array(
+			"name" => "高松 基広",
+			"en" => "Takamatsu Motohiro",
+			"skil" => array(
+				"映像", "Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa / Kashiwa-no-ha"
+			)
+		),
+
+		7 => array(
+			"name" => "辻 雄一郎",
+			"en" => "Tsuji Yuuichirou",
+			"skil" => array(
+				"Scratch", "PHP"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		8 => array(
+			"name" => "西村 三千代",
+			"en" => "Nishimura Sachiyo",
+			"skil" => array(
+				"Scratch"
+			),
+			"dojo" => array(
+				"Kashiwa"
+			)
+		),
+
+		9 => array(
+			"name" => "間下 知紀",
+			"en" => "Mashimo Tomonori",
+			"skil" => array(
+				"Scratch","電子工作"
+			),
+			"dojo" => array(
+				"Kashiwa-no-ha"
+			)
+		)
+		
 	);
 
 
@@ -180,12 +304,12 @@
 
 		</div>
 
-		<div class="mentor-list clearfix">
+		<div class="studentMentor-list clearfix">
 
 			<div class="mentorList-L">
 				<ul class="mentorListUL">
 					<?php
-						for ($i=0; $i < count($mentorData); $i+=2) { 
+						for ($i=0; $i < count($studentMentorData); $i+=2) { 
 							$imgNum = $i + 1;
 
 							print("<li class='mentorListLI'>");
@@ -197,13 +321,13 @@
 							}
 							
 							print("<div class='mentorProfile'>");
-							print("<p class='name'>{$mentorData[$i]['name']}</p>");
-							print("<p class='en'>{$mentorData[$i]['en']}</p>");
-							for ($j=0; $j < count($mentorData[$i]['skil']); $j++) { 
-								print("<p class='skil {$mentorData[$i]['skil'][$j]}'>{$mentorData[$i]['skil'][$j]}");
+							print("<p class='name'>{$studentMentorData[$i]['name']}</p>");
+							print("<p class='en'>{$studentMentorData[$i]['en']}</p>");
+							for ($j=0; $j < count($studentMentorData[$i]['skil']); $j++) { 
+								print("<p class='skil {$studentMentorData[$i]['skil'][$j]}'>{$studentMentorData[$i]['skil'][$j]}");
 							}
-							for ($j=0; $j < count($mentorData[$i]['dojo']); $j++) { 
-								print("<p class='belonging'>{$mentorData[$i]['dojo'][$j]}");
+							for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
+								print("<p class='belonging'>{$studentMentorData[$i]['dojo'][$j]}");
 							}
 							print("</div>");
 							print("</li>");	
@@ -217,7 +341,7 @@
 			<div class="mentorList-R">
 				<ul class="mentorListUL">
 					<?php
-						for ($i=1; $i < count($mentorData); $i+=2) { 
+						for ($i=1; $i < count($studentMentorData); $i+=2) { 
 							$imgNum = $i + 1;
 
 							print("<li class='mentorListLI'>");
@@ -229,13 +353,73 @@
 							}
 							
 							print("<div class='mentorProfile'>");
-							print("<p class='name'>{$mentorData[$i]['name']}</p>");
-							print("<p class='en'>{$mentorData[$i]['en']}</p>");
-							for ($j=0; $j < count($mentorData[$i]['skil']); $j++) { 
-								print("<p class='skil {$mentorData[$i]['skil'][$j]}'>{$mentorData[$i]['skil'][$j]}");
+							print("<p class='name'>{$studentMentorData[$i]['name']}</p>");
+							print("<p class='en'>{$studentMentorData[$i]['en']}</p>");
+							for ($j=0; $j < count($studentMentorData[$i]['skil']); $j++) { 
+								print("<p class='skil {$studentMentorData[$i]['skil'][$j]}'>{$studentMentorData[$i]['skil'][$j]}");
 							}
-							for ($j=0; $j < count($mentorData[$i]['dojo']); $j++) { 
-								print("<p class='belonging'>{$mentorData[$i]['dojo'][$j]}");
+							for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
+								print("<p class='belonging'>{$studentMentorData[$i]['dojo'][$j]}");
+							}
+							print("</div>");
+							print("</li>");	
+
+						}
+					?>
+					
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="adultMentor-list clearfix">
+
+			<div class="mentorList-L">
+				<ul class="mentorListUL">
+					<?php
+						for ($i=0; $i < count($adultMentorData); $i+=2) { 
+							$imgNum = $i + 1;
+
+							print("<li class='mentorListLI'>");
+
+							print("<img src='image/mentorImage/a{$imgNum}.jpg' class='mentorImg'>");
+
+							print("<div class='mentorProfile'>");
+							print("<p class='name'>{$adultMentorData[$i]['name']}</p>");
+							print("<p class='en'>{$adultMentorData[$i]['en']}</p>");
+							for ($j=0; $j < count($adultMentorData[$i]['skil']); $j++) { 
+								print("<p class='skil {$adultMentorData[$i]['skil'][$j]}'>{$adultMentorData[$i]['skil'][$j]}");
+							}
+							for ($j=0; $j < count($adultMentorData[$i]['dojo']); $j++) { 
+								print("<p class='belonging'>{$adultMentorData[$i]['dojo'][$j]}");
+							}
+							print("</div>");
+							print("</li>");	
+
+						}
+					?>
+				</ul>
+			</div>
+
+
+			<div class="mentorList-R">
+				<ul class="mentorListUL">
+					<?php
+						for ($i=1; $i < count($adultMentorData); $i+=2) { 
+							$imgNum = $i + 1;
+
+							print("<li class='mentorListLI'>");
+
+							print("<img src='image/mentorImage/a{$imgNum}.jpg' class='mentorImg'>");
+							
+							print("<div class='mentorProfile'>");
+							print("<p class='name'>{$adultMentorData[$i]['name']}</p>");
+							print("<p class='en'>{$adultMentorData[$i]['en']}</p>");
+							for ($j=0; $j < count($adultMentorData[$i]['skil']); $j++) { 
+								print("<p class='skil {$adultMentorData[$i]['skil'][$j]}'>{$adultMentorData[$i]['skil'][$j]}");
+							}
+							for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
+								print("<p class='belonging'>{$adultMentorData[$i]['dojo'][$j]}");
 							}
 							print("</div>");
 							print("</li>");	
