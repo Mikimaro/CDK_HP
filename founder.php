d32d1ef0de985693e3b3819ce7761df5394ee0cc
mjk0513 <kirie0513@gmail.com>
2016-06-04 23:58:08 +0900

    founder.phpにデータを追加

--- founder.php
+++ founder.php
@@ -2,6 +2,34 @@
 	require_once 'header.php';
 ?>
 
+<div id="container">
+		<div class="pageTitle nonImg">
+			<h1 class="headline">運営について</h1>
+		</div>
+
+		<div id="founder">
+			<p class="title">代表 / Co-Founder</p>
+			<p class="founderImage"><img src="./image/mentorImage/s1.jpg">
+			<p class="founderName">宮島 衣瑛(19)</p>
+
+			<!--
+				細かいデザインを投げますｗ
+				いい感じにしておいてくださいｗ
+			-->
+
+			<p class="founderProfile">
+				1997年5月13日生まれ<br>
+				学習院大学文学部教育学科<br>
+				高校1年生の春から地元である千葉県柏市でCoderDojo Kashiwaという小中学生向けのプログラミングDojoを主催・運営。 3年間で60回を超えるワークショップを開催しており、現在でも様々な場所で教えている。これらの経験を基にTEDxKids@Chiyoda2014に登壇し、自身の考えを発表した。2016年4月からは教育学科に進学し、より専門的に教育について学んでいる。
+			</p>
+
+			<a href="https://www.youtube.com/watch?v=pvEbHI3estc">TEDxKids@Chiyoda2014の映像を見る</a>
+			<a href="http://workmill.jp/20160531_teen1.html">対談記事を見る</a>
+
+		</div>
+
+</div>
+
 <?php
 	require_once 'footer.php';
 ?>
