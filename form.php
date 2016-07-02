f31de7d041437fcb51265b22009d480349ef6f5a
mjk0513 <kirie0513@gmail.com>
2016-07-03 01:25:46 +0900

    フォームに表示する内容を変数で管理できるようにした。

--- form.php
+++ form.php
@@ -1,7 +1,12 @@
 <?php
 	$flag = $_POST['flag'];
 
-			
+	//pageShowFlag
+	//0:申し込みフォームオープン
+	//1:〜1週間前クローズ
+	//2:定員オーバークローズ
+
+	$pageShowFlag = 1;
 
 	if ($flag == 1) {
 
@@ -62,7 +67,9 @@
 
 <div id="container">
 	
-	<div class="pageTitle nonImg">
+	<div class="pageTitle">
+
+		<img src="./image/dojoLogo.png" class="headerLogo">
 
 		<h1 class="headline">参加申し込みフォーム</h1>
 
@@ -72,24 +79,27 @@
 			
 		</p>
 
-		<p class="formAttention ">上限に達した時点で予約不可となっております。</p>
-
 	</div>
 
-	<div id="join">
+<?php
+	if ($pageShowFlag == 0) {
+print<<<EOF
 
-		<form action="" method="POST">
+	<div id='join'>
 
-			<div class="individual title-of-top">
+		<form action=' method='POST'>
 
-			<p class="formTitle">お名前</p>
-				<p class="errorMsg"><?php print($error['childName']);?></p>
-				<input type="text" name="childName" class="textBox">
+			<div class='individual title-of-top'>
 
-			<p class="formTitle">学年</p>
-				<p class="errorMsg"><?php print($error['grade']);?></p>
-				<select name="grade">
-					<?php
+			<p class='formTitle'>お名前</p>
+				<p class='errorMsg'>{$error['childName']}</p>
+				<input type='text' name='childName' class='textBox'>
+
+			<p class='formTitle'>学年</p>
+				<p class='errorMsg'>{$error['grade']}</p>
+				<select name='grade'>
+EOF;
+
 						for ($i=1; $i < 7; $i++) { 
 							print("<option value='小学{$i}年生'>小学{$i}年生</option>");
 						}
@@ -97,30 +107,65 @@
 						for ($i=1; $i < 4; $i++) { 
 							print("<option value='中学{$i}年生'>中学{$i}年生</option>");
 						}
-					?>
-					<option value="その他">その他</option>
+print<<<EOF
+					<option value='その他'>その他</option>
 				</select>
 
-			<p class="formTitle">メールアドレス</p>
-				<p class="errorMsg"><?php print($error['mail']);?></p>
-				<input type="text" name="mail" class="textBox">
+			<p class='formTitle'>メールアドレス</p>
+				<p class='errorMsg'>{$error['mail']}</p>
+				<input type='text' name='mail' class='textBox'>
 
-			<p class="formTitle">ノートパソコンの貸出を希望しますか？</p>
-				<p class="errorMsg"><?php print($error['pcRent']);?></p>
-				<input type="radio" name="pcRent" value="希望する" checked="">希望する
-				<input type="radio" name="pcRent" value="希望しない">希望しない
+			<p class='formTitle'>ノートパソコンの貸出を希望しますか？</p>
+				<p class='errorMsg'>{$error['pcRent']}</p>
+				<input type='radio' name='pcRent' value='希望する' checked='>希望する
+				<input type='radio' name='pcRent' value='希望しない'>希望しない
 
-			<p class="formTitle">備考（あらかじめ伝えておきたいこと）</p>
-				<textarea cols="50" rows="10" name="option"></textarea>
+			<p class='formTitle'>備考（あらかじめ伝えておきたいこと）</p>
+				<textarea cols='50' rows='10' name='option'></textarea>
 
-				<input type="hidden" name="flag" value="1">
+				<input type='hidden' name='flag' value='1'>
 
-				<div class="space"><input type="submit" value="申し込む"></div>
+				<div class='space'><input type='submit' value='申し込む'></div>
 
 			</div>
 		</form>
 
 	</div>
+
+EOF;
+	} elseif ($pageShowFlag == 1) {
+
+print<<<EOF
+	
+	<div id='fullUp'>
+		<p>次回のCoderDojo Kashiwaは定員に達したため申し込みを締め切らせていただきます。<br>
+		またのご参加をお待ちしております。</p>
+		2016年度のCoderDojo Kashiwaの運営体制について><a href="./news/">さらに詳しく </a>
+		<br>
+	</div>
+
+	<br><br>
+
+EOF;
+
+	} elseif ($pageShowFlag == 2) {
+
+print<<<EOF
+	
+	<div id='fullUp'>
+		<br>
+		<p>申し込みフォームは開催の１週間前にオープンしております。もうしばらくお待ち下さい。</p>
+		<br>
+	</div>
+
+	<br><br>
+
+EOF;
+
+	}
+?>
+
+
 
 </div>
 
