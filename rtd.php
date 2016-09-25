0ce591ebcc99a8eedbb19c43c99ba95a78964bc5
Mikimaro Yonekura <MikimaroYonekura@Mikimaro-no-MacBook-Pro.local>
2016-09-25 16:14:07 +0900

    rtd page fixed

--- rtd.php
+++ rtd.php
@@ -22,44 +22,114 @@
 
 				<p class="text">
 					RTD(Real Time Documentation)とは、ワークショップやイベントの出来事を映像で記録し、それらの終了時に編集・保存されたメディアとして公開できるパーソナルメディア活動です。<br>
-
-					RTDには3つの表現方法があります。<br>
-				</p>
-
-				<ul>
-
-					<?php
-						$rtdType = array("Real Time Paper（リアルタイムペーパー）","Real Time Video（リアルタイムビデオ）","Document Wall（ドキュメントウォール）");
-
-						for ($i =0; $i <count($rtdType) ; $i++) { 
-							print("<li>{$rtdType[$i]}</li>");
-						}
-					?>
-				</ul>
-
-				<br>
-
-				<p class="text">
-					詳しくは以下のサイトで紹介しています。<br>
-
-					・<a href="http://coderdojo-kashiwa.com/rtd/" target="blank_">RTD公式サイト</a>
-
 				</p>
 
 			</div>
 
-			<br>
 
-			<div>
-				<p class="title">CoderDojo Kashiwa/Kashiwa-no-haでの活動</p>
+			<div class="video">
 
-				<p class="text">
-					私達の活動は2014年から始まりました。<br><br>
-					
+				<p class="title">WORKS</p>
 
-				</p>
+				<p class="text">私達の今まで作品の一部です。</p>
+
 			</div>
+
 		</div>
+
+		<section class="rtdmovie">
+
+			<article>
+
+				<iframe width="853" height="480" src="https://www.youtube.com/embed/XyOeJ7Uo9zA" frameborder="0" allowfullscreen></iframe>
+
+				<section>
+
+					<p class="movieTitle">Happy birthday CoderDojo! from CoderDojo Kashiwa</p>
+
+					<p class="date">2016年8月28日</p>
+
+
+					<p class="text">第65回CoderDojo Kashiwaが開催されました。</p>
+
+				</section>
+
+			</article>
+			
+		</section>
+
+		<section class="movielist">
+			
+			<article>
+
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/Ct8EKh8xXWQ" frameborder="0" allowfullscreen></iframe>
+
+				<section>
+
+					<p class="movieTitle"> #65 CoderDojo Kashiwa</p>
+
+					<p class="date">2016年8月28日</p>
+
+
+					<p class="text">第65回CoderDojo Kashiwaが開催されました。</p>
+
+				</section>
+
+			</article>
+
+			<article>
+
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/gUJqUK6oTwY" frameborder="0" allowfullscreen></iframe>
+
+				<section>
+
+					<p class="movieTitle">ScratchDay in Kashiwa 2016</p>
+
+					<p class="date">2016年5月14日</p>
+
+
+					<p class="text">千葉県で初のScratchDay in Kashiwaが開催されました。</p>
+
+				</section>
+
+			</article>
+
+			<article>
+
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/ScEu7O4y6cY" frameborder="0" allowfullscreen></iframe>
+
+				<section>
+
+					<p class="movieTitle">Special Presentation Day 2015</p>
+
+					<p class="date">2015年12月20日</p>
+
+
+					<p class="text">年に1度の子供たちによる、作品発表会が開催されました。</p>
+
+				</section>
+
+			</article>
+
+			<article>
+
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/wmG1D5AnHno" frameborder="0" allowfullscreen></iframe>
+
+				<section>
+
+					<p class="movieTitle">CoderDojo Kahsiwa-no-ha Opening</p>
+
+					<p class="date">2015年6月7日</p>
+
+
+					<p class="text">CoderDojo Kashiwaに姉妹Dojoが誕生しました。</p>
+
+				</section>
+
+			</article>
+
+		</section>
+
 	</div>
 </div>
 
