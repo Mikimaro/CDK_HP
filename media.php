3cc944ee7cab4ae502337465375bb89aa068b547
Kirie MIyajima <kirie0513@gmail.com>
2016-10-20 19:42:33 +0900

    メディア掲載情報を更新

--- media.php
+++ media.php
@@ -2,24 +2,37 @@
 	require_once 'header.php';
 
 	$title = array(
+		"子どものための無料プログラミング道場「CoderDojo」をはじめてみませんか",
+		"柏市教職員夏季情報活用研修講座　取材レポート",
+		"「自由」というルールを共有し、プログラミングの学習機会を提供する
+CoderDojo Japan",
 		"道場するから 来ておくれ",
 		"好評！ 高校生が小中学生にプログラミング技術伝授",
 		"TKPシアターで上映する映画を子供たちが作る！CoderDojoKashiwa特別ワークショップレポート #TKP×CDK"
 	);
 
 	$date = array(
+		"2016.10.27",
+		"2016.10.18",
+		"2016.9.30",
 		"2015.06.24",
 		"2014.02",
 		"2014.02.26"
 	);
 
 	$description = array(
+		"ICT教育ニュースにCoderDojo Japan及びCoderDojo Kashiwa, Kodairaが掲載されました。",
+		"ベネッセ教育総合研究所の「まなびのかたち」にて、柏市のプログラミング教育事業に関する取材記事が掲載され、CoderDoj o Kashiwaについてもご紹介頂きました。",
+		"ベネッセ教育総合研究所の「まなびのかたち」にて、CoderDojo JapanとCoderDojo Kashiwaの取り組みが紹介されました。",
 		"CoderDojo Kashiwa-no-haの様子が柏市の公式ブログに掲載されました。",
 		"2014年2月に開催したTKPシアター×CoderDojo Kashiwa特別ワークショップの開催レポートが、朝日れすか様に掲載されました。",
 		"2014年2月に開催したTKPシアター×CoderDojo Kashiwa特別ワークショップの開催レポートが、CodeIQ MAGAZINE様に掲載されました。"
 	);
 
 	$links = array(
+		"http://ict-enews.net/zoomin/17coderdojo/",
+		"http://berd.benesse.jp/special/manabi/manabi_11.php",
+		"http://berd.benesse.jp/special/manabi/manabi_10.php",
 		"http://www.city.kashiwa.lg.jp/soshiki/blogtokumei/p023808.html",
 		"http://www.resuka.co.jp/news/2014/04.html",
 		"https://codeiq.jp/magazine/2014/02/6389/"
