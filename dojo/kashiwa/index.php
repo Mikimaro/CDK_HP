729edd48cc07ff98dfc652f1ba46f6c597618cec
Kirie MIyajima <kirie0513@gmail.com>
2017-06-06 16:23:28 +0900

    各DojoのDetailページのデザインができた

--- dojo/kashiwa/index.php
+++ dojo/kashiwa/index.php
@@ -1,6 +1,6 @@
 <?php
     $pageTitle = "Kashiwa";
-    require_once "header.php";
+    require_once "../common/header.php";
 ?>
 
 <div id="container">
@@ -20,21 +20,47 @@
 
             <img src="./images/logo.png" class="dojoDetailLogo">
 
-            <ul>
+            <ul class="dojoDetailUl">
                 <li>名称：CoderDojo Kashiwa</li>
                 <li>場所：<a href="http://nbob.jp/" target="_blank">Noblesse Oblige</a></li>
                 <li>住所：千葉県柏市東上町2-28 第一水戸屋ビル3F</li>
+                <li>開催日：毎月第2・4日曜日 10:00-12:00</li>
                 <li>対象：柏市近辺に住んでいるすべての小学3年生〜中学3年生</li>
                 <li>定員：23名</li>
+                
+                <li>学べること：</li>
             </ul>
+
+            <ul class="dojoDetailSkill">
+                <li class="dojoDetailSkillScratch">Scratch</li>
+                <li class="dojoDetailSkillWeb">web</li>
+                <li class="dojoDetailSkillDesign">デザイン</li>
+            </ul>
+
+            <a href="#" class="joinButton">参加する</a>
+
         </div>
 
         <div id="dojoDetailImageArea">
-            <img src="./images/bg01.jpg" width="100%" class="dojoDetailImage">
+            <div class="flexslider">
+                <ul class="slides">
+                    <li><img src="./images/bg01.jpg" class="dojoDetailImage"></li>
+                    <li><img src="./images/bg02.jpg" class="dojoDetailImage"></li>
+                    <li><img src="./images/bg03.jpg" class="dojoDetailImage"></li>
+                    <li><img src="./images/bg04.jpg" class="dojoDetailImage"></li>
+                    <li><img src="./images/bg05.jpg" class="dojoDetailImage"></li>
+                </ul>
+            </div>
+
         </div>
+
+       
+
     </div>
 
 </div>
+
+
 
 <?php
     require_once "footer.php";
