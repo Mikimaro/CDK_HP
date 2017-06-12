360ca25a5505d21a040fa02a7fc216577261eab7
Mikimaro Yonekura <mikimaroyonekura@MYonekura-MacBook.local>
2017-06-12 10:17:14 +0900

    競合解決したかな

--- dojo/nankashi/index.php
+++ dojo/nankashi/index.php
@@ -1,10 +1,6 @@
 <?php
     $pageTitle = "Minami-Kashiwa";
-<<<<<<< HEAD
-    require_once "header.php";
-=======
     require_once "../common/header.php";
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
 ?>
 
 <div id="container">
@@ -25,33 +21,18 @@
             <img src="./images/logo.png" class="dojoDetailLogo">
 
             <ul class="dojoDetailUl">
-<<<<<<< HEAD
-                <li>名称：CoderDojo Minami-Kashwia</li>
-                <li>場所：<a href="http://nbob.jp/" target="_blank"></a></li>
-=======
                 <li>名称：CoderDojo Minami-Kashiwa</li>
                 <li>場所：<a href="#" target="_blank">調整中</a></li>
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
                 <li>住所：調整中</li>
                 <li>開催日：調整中</li>
                 <li>対象：柏市近辺に住んでいるすべての小学3年生〜中学3年生</li>
                 <li>定員：調整中</li>
                 
                 <li>学べること：</li>
-                 <li>名称：CoderDojo Minami-Kashiwa</li>
-                 <li>場所：<a href="#" target="_blank">調整中</a></li>
-                 <li>住所：調整中</li>
-                 <li>開催日：調整中</li>
-                 <li>対象：柏市近辺に住んでいるすべての小学3年生〜中学3年生</li>
-                 <li>定員：調整中</li>
             </ul>
 
             <ul class="dojoDetailSkill">
                 <li class="dojoDetailSkillScratch">Scratch</li>
-<<<<<<< HEAD
-=======
-                
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
             </ul>
 
             <a href="#" class="joinButton">参加する</a>
--- dojo/shounan-machi/index.php
+++ dojo/shounan-machi/index.php
@@ -8,11 +8,7 @@
 		<h1 class="headline">CoderDojo Minami-Kashiwa</h1>
 
 		<p class="headline">
-<<<<<<< HEAD
-			旧沼南町に初めてのDojoです。
-=======
 			旧沼南町エリアでこの夏スタート。
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
 		</p>
 
 	</div>
@@ -26,11 +22,7 @@
 
             <ul class="dojoDetailUl">
                 <li>名称：CoderDojo Shounan-machi</li>
-<<<<<<< HEAD
-                <li>場所：<a href="http://nbob.jp/" target="_blank"></a></li>
-=======
                 <li>場所：<a href="#" target="_blank">調整中</a></li>
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
                 <li>住所：調整中</li>
                 <li>開催日：調整中</li>
                 <li>対象：柏市近辺に住んでいるすべての小学3年生〜中学3年生</li>
@@ -41,10 +33,6 @@
 
             <ul class="dojoDetailSkill">
                 <li class="dojoDetailSkillScratch">Scratch</li>
-<<<<<<< HEAD
-=======
-                
->>>>>>> 653311af852c3c712e875d5c951fcc1eacf7cbf1
             </ul>
 
             <a href="#" class="joinButton">参加する</a>
