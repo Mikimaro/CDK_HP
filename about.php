87d4166449050df12c3b6a13da3808ed88922e39
Kirie MIyajima <kirie0513@gmail.com>
2017-06-29 10:30:15 +0900

    コンテンツを大幅追加

--- about.php
+++ about.php
@@ -24,57 +24,139 @@
 
 		<div id="missionMenu">
 			<ul>
-				<li><a href="#about">CoderDojoとは</a></li>
+				<li><a href="#coderdojo">CoderDojoとは</a></li>
 				<li><a href="#history">私たちの歴史</a></li>
 				<li><a href="#project">プロジェクト</a></li>
-				<li><a href="#leadership">リーダーシップ</a></li>
+				<li><a href="#champion">チャンピオン</a></li>
 			</ul>
 		</div>
 
 		<div id="about">
+			<div id="coderdojo">
+				<p class="missionTitle">CoderDojoとは</p>
+				
+				<div class="leftContents">
+					<img src="./image/dojoLogo.png" class="missionLogo">
+				</div>
 
-			<div class="individual">
+				<div class="rightContents">
 
-				<div class="aboutCoderDojo title-of-top">
-
-					<p class="title">CoderDojoのはじまり</p>
-					<p class="text">
-						CoderDojoとは2011年にアイルランドから始まった小・中学生向けの無料プログラミングDojoです。<br>
-						現在では63カ国で875以上のDojoが開催されています。<br>
-						・<a href="https://coderdojo.com/" target="blank_">CoderDojo公式サイト</a>
+					<p class="missionText">
+						CoderDojo は7〜17歳の子どもを対象にしたプログラミング道場です。2011年にアイルランドで始まり、世界では69カ国・1,200の道場、日本では全国に88以上の道場があります。<br>
+						CoderDojoはプログラミング教室ではありません。子どもたちは自分のプロジェクトを完成させるためにDojoに来ます。<br>
 					</p>
+
+					<ul>
+						<li><a href="http://coderdojo.jp" target="_blank">CoderDojo Japanについて</a></li>
+						<li><a href="http://coderdojo.com" target="_blank">CoderDojo Foundationについて</a></li>
+					</ul>
+
+				</div>
+				<!--
+				<div class="missionVideoArea">
+					<iframe width="90%" height="500" src="https://www.youtube.com/embed/gLDue2xb1j8?rel=0" frameborder="0" allowfullscreen class="missionVideo"></iframe>
+				</div>
+				-->
+			</div>
+
+			<div id="history">
+				<p class="missionTitle">私たちの歴史</p>
+
+				<div class="left70Contents">
+					<p class="missionText">
+						CoderDojo Kashiwaは2013年5月にスタートしました。第1回のDojoは4名の参加者でしたが、回数を重ねていくにつれて徐々に人数が増え始め、現在では1時間以内に定員に達してしまうほどになりました。
+					</p>
+				</div>
+
+				<div class="right30Contents">
+					<p>ここにイメージ</p>
+				</div>
+
+			</div>
+
+			<div id="project">
+				<p class="missionTitle">プロジェクト</p>
+
+				<div class="spd">
+
+					<table border="0" class="missionTable">
+						<tr>
+							<td class="missionTdL">
+								<img src="./image/eventLogos/spd2016.png" class="missionLogoSpd">
+							</td>
+
+							<td class="missionTdR">
+								<p class="missionSubTitle">Special Presentation Day</p>
+								<p class="missionText">
+									年に一度の発表会です。子どもたちが作った作品を自分自身で発表します。
+								</p>
+								<a href="event/spd" target="_blank">Special Presentation Day について更に詳しく</a>
+							</td>
+						</tr>
+					</table>
 
 				</div>
 
+				<div class="scratchday">
+					
+					<table border="0" class="missionTable">
+						<tr>
+							<td class="missionTdL">
+								<img src="./image/sdklogo.png" class="missionLogo">
+							</td>
+
+							<td class="missionTdR">
+								<p class="missionSubTitle">ScratchDay in Kashiwa</p>
+								<p class="missionText">
+									ScratchDayとは、MITメディアラボLLKグループが開発したプログラミングツールであるScratchの誕生日を祝うイベントです。毎年5月に全世界同時に行われています。<br>
+									柏では2016年からScratchDayを開催しています。初心者向けのワークショップやShow&Tellなど、はじめて来てもらう人も楽しめるイベントになっています。
+								</p>
+								<a href="http://scratchday-kashiwa.org" target="_blank">ScratchDayについて更に詳しく</a>
+							</td>
+						</tr>
+						
+					</table>
+
+				</div>
 				
+				<div class="kashiwacity">
 
-				<div class="aboutCDK">
+					<table border="0" class="missionTable">
+						<tr>
+							<td class="missionTdL">
+								<img src="./image/kashiwacity.png" class="missionLogo">
+							</td>
 
-					<p class="title">CoderDojo Kashiwa</p>
+							<td class="missionTdR">
 
-					<!-- Twitter -->
-					<div>
-						<a href="https://twitter.com/C_D_Kashiwa" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false" data-dnt="true">Follow</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+								<p class="missionSubTitle">柏市プログラミング教育</p>
+								<p class="missionText">
+									柏市では2017年4月から市内にある42校すべての小学校でプログラミング教育をスタートしました。私たちはカリキュラム提供とアドバイザーという役目で協力しています。<br>
+									学校で体験するのはあくまでも入門です。プログラミングを体験して、さらに深めたいと思った子どもたちをCoderDojo Kashiwaは全力でサポートします。
+								</p>	
+								
+							</td>
+						</tr>
+						
+					</table>
+
+
+					<div class="leftContents">
+					</div>
+
+					<div class="rightContents">
+						
 
 					</div>
 
-					<p class="text">
-						CoderDojo Kashiwaは2013年5月5日に第１回を開催しました。<br>
-					
-						参加者は４人でとてもこじんまりとした会でした。ですが会を重ねるにつれて子どもたちもメンターも増え、今では平均して18名以上が参加する大規模なDojoとなりました。<br><br>
-
-						また、CoderDojo Kashiwaの特徴は運営を学生が行っていることです。創設メンバーは小学生のときから友人である2名です。<br>
-						メンター（教える人）も多くは高校生ですが、プロのプログラマである大人も一緒に教えています。<br><br>
-
-						現在、様々な協力があり、60回以上開催してきました。そして、姉妹Dojoの<a href="kashiwanoha">CoderDojo Kashiwa-no-ha</a>が2015年6月に柏の葉に誕生しました。<br><br>
-
-						そしてCoderDojo Kashiwaは2016年5月で3年を迎えました。その記念すべき年に、私たちは「<a href="event/ScratchDay">ScratchDay in Kashiwa</a>」を開催しました。ScratchDayとは、アメリカのマサチューセッツ工科大学（MIT）メディアラボが開発した教育用プログラミングツール”Scratch”の誕生を祝うお祭りです。
-					</p>
-
 				</div>
 
-				
 			</div>
+
+			<div id="champion">
+				<p class="missionTitle">チャンピオン</p>
+			</div>
+
 		</div>
 	</div>
 </div>
