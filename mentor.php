d7fd76a2b98d1ce6f21627d63d0513b26789eb8a
Kirie MIyajima <kirie0513@gmail.com>
2017-06-29 12:07:35 +0900

    メンターページの大幅改定

--- mentor.php
+++ mentor.php
@@ -1,560 +1,66 @@
 <?php
-	$studentMentorData = array(
-		0 => array(
-			"name" => "宮島 衣瑛",
-			"en" => "Miyajima Kirie",
-			"skil" => array(
-				"Scratch", "Swift", "Web", "PHP"
-			),
-			"dojo" =>array(
-				"Kashiwa / Kashiwa-no-ha"
-			),
-			"comment" => "",
-			"positon" => "CoderDojo柏代表"
-		),
-
-		1 => array(
-			"name" => "加藤 遼",
-			"en" => "Katou Haruka",
-			"skil" => array(
-				"Scratch", "Web"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => "",
-			"positon" => "CoderDojo柏代表"
-		),
-
-		2 => array(
-			"name" => "鈴木 貴也",
-			"en" => "Suzuki Takaya",
-			"skil" => array(
-				"Scratch","音響"
-			),
-			"dojo" => array(
-				"Kashiwa / Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		3 => array(
-			"name" => "飯島 巧",
-			"en" => "Iijima Takumi",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa / Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		4 => array(
-			"name" => "牧野 翔",
-			"en" => "Makino Syou",
-			"skil" => array(
-				"Scratch","音響"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		5 => array(
-			"name" => "北原 成貴",
-			"en" => "Kitahara Masaki",
-			"skil" => array(
-				"映像","Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => "Dojoで写真や映像を撮ったりしています。<br>
-			現在、美術大学に通い、多種多様な芸術分野について学んでいます。<br>
-			何かわからない事があれば聞いてください。",
-			"positon" => "広報"
-		),
-
-		6 => array(
-			"name" => "米倉 幹麿",
-			"en" => "Yonekura Mikimaro",
-			"skil" => array(
-				"映像", "Scratch", "Web"
-			),
-			"dojo" => array(
-				"Kashiwa / Kashiwa-no-ha"
-			),
-			"comment" => "CoderDojoKashiwaでは、写真や映像を撮っています。<br>
-			メンター＜カメラマンというパワーバランスで活動しています。<br>",
-			"positon" => "カメラマン"
-		),
-
-		7 => array(
-			"name" => "成澤 巧徒",
-			"en" => "Narisawa Takuto",
-			"skil" => array(
-				"Scratch", "電子工作", "Java"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		8 => array(
-			"name" => "相良 亮",
-			"en" => "Sagara Ryou",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		9 => array(
-			"name" => "竹内 遥斗",
-			"en" => "Takeuchi Haruto",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		10 => array(
-			"name" => "村脇 光洋",
-			"en" => "Murawaki Mitsuhiro",
-			"skil" => array(
-				"Scratch", "Swift", "JavaScript"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		11 => array(
-			"name" => "福田 敏史",
-			"en" => "Hukuda Toshihumi",
-			"skil" => array(
-				"Scratch", "Java"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		12 => array(
-			"name" => "窪田 碧唯",
-			"en" => "Kubota Aoi",
-			"skil" => array(
-				"Scratch","デザイン"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => "",
-			"positon" => "CoderDojo柏の葉代表"
-		),
-
-		13 => array(
-			"name" => "大熊 謙輔",
-			"en" => "Ookura Kensuke",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		14 => array(
-			"name" => "柄澤 匠",
-			"en" => "Karasawa Takumi",
-			"skil" => array(
-				"Scratch","Web","Phyton"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => "",
-			"positon" => "CoderDojo柏の葉代表"
-		),
-
-		15 => array(
-			"name" => "若山 広大",
-			"en" => "Wakayama Kouta",
-			"skil" => array(
-				"JavaScript"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-
-	);
-
-	$adultMentorData = array(
-		0 => array(
-			"name" => "石原 怜",
-			"en" => "Ishihara Ryou",
-			"skil" => array(
-				"Scratch", "Objective-C"
-			),
-			"dojo" =>array(
-				"Kashiwa / Kashiwa-no-ha"
-			),
-			"comment" => ""
-		),
-
-		1 => array(
-			"name" => "遠藤 貴士",
-			"en" => "Endou Takashi",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => "いつもはモノづくりの会社向けにプログラムを作っています。<br>
-			作ったプログラムが思ったとおりに動かないときは、「なんでだろう？」と考えながら何回もためしてみてください。<br>
-			だんだん思いに近づいてくるのが楽しくなります。<br>
-			プログラミングを楽しみながら「あきらめない人」をめざしましょう。おうえんします！"
-		),
-
-		2 => array(
-			"name" => "小野寺 宣裕",
-			"en" => "Onodera Nobuhiro",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		3 => array(
-			"name" => "鎌田 泰行",
-			"en" => "Kamata Yasuyuki",
-			"skil" => array(
-				"Scratch","Unity"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		4 => array(
-			"name" => "坂田 健一",
-			"en" => "Sakata Kenchi",
-			"skil" => array(
-				"Scratch","ActionScript","PHP","C"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => "〈心掛けていること〉 <br>
-			・Coder Dojo を通じてプログラミングでコンピューターをコントロールする楽しさを体験してもらう<br>
-			・子供同士のコミュニケーションから年齢を越えた人同士のやりとりから学校では得づらい経験をしてもらう  
-			<br><br>
-			〈参加される方へ〉<br>
-			プログラミングをやってみたいなぁと思っている方はもちろん、どんなものかよくわからないけど、<br>
-			最近聞いたことあるなぁと思っている方、試しに来てみてください。<br>
-			その後、おもしろそうなら、また来てください"
-		),
-
-		5 => array(
-			"name" => "櫻庭 敬之",
-			"en" => "Sakuraba Noriyuki",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		6 => array(
-			"name" => "澤 孝治",
-			"en" => "Sawa Takaharu",
-			"skil" => array(
-				"映像","Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		7 => array(
-			"name" => "辻 雄一郎",
-			"en" => "Tsuji Yuuichirou",
-			"skil" => array(
-				"Scratch", "PHP"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		8 => array(
-			"name" => "西村 三千代",
-			"en" => "Nishimura Sachiyo",
-			"skil" => array(
-				"Scratch"
-			),
-			"dojo" => array(
-				"Kashiwa"
-			),
-			"comment" => ""
-		),
-
-		9 => array(
-			"name" => "間下 知紀",
-			"en" => "Mashimo Tomonori",
-			"skil" => array(
-				"Scratch","電子工作"
-			),
-			"dojo" => array(
-				"Kashiwa-no-ha"
-			),
-			"comment" => ""
-		)
-		
-	);
-
-
-	
-
 	require_once 'header.php';
-
-
 ?>
 
 <div id="container">
 
-		<div class="pageTitle">
-			<img src="./image/dojoLogo.png" class="headerLogo">
-			<h1 class="headline">メンター</h1>
-			<p class="headline">子どもたちひとりひとりと向き合う、CoderDojo Kashiwaのエキスパートたちを紹介します</p>
-
-		</div>
-
-		<div id="mentor">
-
-		<div class="aboutMentor title-of-top">
-
-			<div class="individual">
-				
-				<p class="title">メンターとは</p>
-				<p class="text"> 	
-					CoderDojo Kashiwaでは、先生のことをメンターと呼んでいます。これには"共に考え、共に学ぶ"という意味が込められています。<br>
-					子どもたち１人１人と向き合い、丁寧かつ繊細なサポートができる。これこそがCoderDojo Kashiwaの強みです。<br>
-					メンターになる方法は簡単です。<br>
-					詳しくは<a href="adopt.php">メンター募集</a>をご覧ください。
-				</p>
-
-				<br>
-
-			</div>
-
-		</div>
-
-		<div id="segment">
-			
-			<h5 class="choose"><a href="javascript:;" onclick="Display('no1')">学生メンター</a></h5>
-
-			<h5 class="choose"><a href="javascript:;" onclick="Display('no2')">一般メンター</a></h5>
-
-		</div>
-
-		<div id="mentorList">
-
-			<div id="studentMentor-list" class="clearfix">
-
-				<div class="mentorList-L">
-
-					<ul class="mentorListUL">
-
-						<?php
-							for ($i=0; $i < count($studentMentorData); $i+=2) { 
-								$imgNum = $i + 1;
-
-								print("<li class='mentorListLI'>");
-
-									print("<a data-target='modalStudent{$imgNum}' class='modal-open'><img src='image/mentorImage/s{$imgNum}.jpg' class='mentorImg'></a>");
-							
-									print("<div class='mentorProfile'>");
-									print("<p class='name'>{$studentMentorData[$i]['name']}</p>");
-									print("<p class='en'>{$studentMentorData[$i]['en']}</p>");
-								for ($j=0; $j < count($studentMentorData[$i]['skil']); $j++) { 
-									print("<p class='skil {$studentMentorData[$i]['skil'][$j]}'>{$studentMentorData[$i]['skil'][$j]}");
-								}
-								for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
-									print("<p class='belonging'>{$studentMentorData[$i]['dojo'][$j]}");
-								}
-									print("</div>");
-								print("</li>");	
-
-							}
-						?>
-					</ul>
-				</div>
-
-
-			<div class="mentorList-R">
-				<ul class="mentorListUL">
-					<?php
-						for ($i=1; $i < count($studentMentorData); $i+=2) { 
-							$imgNum = $i + 1;
-
-							print("<li class='mentorListLI'>");
-
-								print("<a data-target='modalStudent{$imgNum}' class='modal-open'><img src='image/mentorImage/s{$imgNum}.jpg' class='mentorImg'></a>");
-														
-								print("<div class='mentorProfile'>");
-								print("<p class='name'>{$studentMentorData[$i]['name']}</p>");
-								print("<p class='en'>{$studentMentorData[$i]['en']}</p>");
-							for ($j=0; $j < count($studentMentorData[$i]['skil']); $j++) { 
-								print("<p class='skil {$studentMentorData[$i]['skil'][$j]}'>{$studentMentorData[$i]['skil'][$j]}");
-							}
-							for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
-								print("<p class='belonging'>{$studentMentorData[$i]['dojo'][$j]}");
-							}
-								print("</div>");
-							print("</li>");	
-
-						}
-					?>
-					
-				</ul>
-			</div>
-
-			</div>
-
-			<div id="adultMentor-list" class="clearfix">
-
-			<div class="mentorList-L">
-				<ul class="mentorListUL">
-					<?php
-						for ($i=0; $i < count($adultMentorData); $i+=2) { 
-							$imgNum = $i + 1;
-
-							print("<li class='mentorListLI'>");
-
-							print("<a data-target='modalAdult{$imgNum}' class='modal-open'><img src='image/mentorImage/a{$imgNum}.jpg' class='mentorImg'></a>");
-
-							print("<div class='mentorProfile'>");
-							print("<p class='name'>{$adultMentorData[$i]['name']}</p>");
-							print("<p class='en'>{$adultMentorData[$i]['en']}</p>");
-							for ($j=0; $j < count($adultMentorData[$i]['skil']); $j++) { 
-								print("<p class='skil {$adultMentorData[$i]['skil'][$j]}'>{$adultMentorData[$i]['skil'][$j]}");
-							}
-							for ($j=0; $j < count($adultMentorData[$i]['dojo']); $j++) { 
-								print("<p class='belonging'>{$adultMentorData[$i]['dojo'][$j]}");
-							}
-							print("</div>");
-							print("</li>");	
-
-						}
-					?>
-				</ul>
-			</div>
-
-
-			<div class="mentorList-R">
-				<ul class="mentorListUL">
-					<?php
-						for ($i=1; $i < count($adultMentorData); $i+=2) { 
-							$imgNum = $i + 1;
-
-							print("<li class='mentorListLI'>");
-
-							print("<a data-target='modalAdult{$imgNum}' class='modal-open'><img src='image/mentorImage/a{$imgNum}.jpg' class='mentorImg'></a>");
-							
-							print("<div class='mentorProfile'>");
-							print("<p class='name'>{$adultMentorData[$i]['name']}</p>");
-							print("<p class='en'>{$adultMentorData[$i]['en']}</p>");
-							for ($j=0; $j < count($adultMentorData[$i]['skil']); $j++) { 
-								print("<p class='skil {$adultMentorData[$i]['skil'][$j]}'>{$adultMentorData[$i]['skil'][$j]}");
-							}
-							for ($j=0; $j < count($studentMentorData[$i]['dojo']); $j++) { 
-								print("<p class='belonging'>{$adultMentorData[$i]['dojo'][$j]}");
-							}
-							print("</div>");
-							print("</li>");	
-
-						}
-					?>
-					
-				</ul>
-			</div>
-
-			</div>
-
-		</div>
-
+	<div id="mentorImageArea">
+		<img src="./image/mentorImage/mentor.jpg" class="missionImage">
+		<p class="titleUp">メンター</p>
+		<p class="titleMiddle">子どもたちひとりひとりと向き合う、<br>CoderDojo Kashiwaのエキスパートです。</p>
 	</div>
 
+	<div id="mentor">
+
+		<div id="aboutMentor">
+			<br>
+			<p class="title">メンターとは</p>
+
+			<p class="mentorText">
+				メンターとは、CoderDojo Kashiwaの先生です。これには"共に考え助ける"という意味合いがあり、ただ自分の知識を子どもたちに一方的に共有するような"先生"とは違うという意味でこの言葉を使っています。<br>
+				CoderDojo Kashiwaには様々なメンターがいます。学生メンターはもちろん、仕事でプログラムしている大人もたくさんいます。<br>
+				私たちの活動は完全なボランティアで、メンターたちは無給で教えています。また、活動が月2回あることもあり少しハードルが高いように思えてしまうかもしれません。しかし、CoderDojoという場所はなにかに縛られながら活動するわけではありません。自分の都合がつくときにだけでもメンターとして参加してくださるだけで構わないのです。あなたの力をCoderDojo Kashiwaで発揮してみませんか。
+			</p>
+
+			<p class="title">メンターとしての素質</p>
+				<p class="text">私たちは以下のような方にメンターになっていただきたいと考えています。
+					<ul>
+						<li>子どもたちのことが好き</li>
+						<li>コンピュータが好き</li>
+						<li>教えることが好き</li>
+						<li>仕事でプログラムを書いている</li>
+						<li>教育関係に興味がある</li>
+					</ul>
+				</p>
+
+				<p class="title">活動概要</p>
+				<p class="text">CoderDojo Kashiwa及びKashiwa-no-haでの活動概要です。</p>
+				<p class="text">【Kashiwa】</p>
+				<ul>
+					<li>活動日時：毎月第2・4日曜日　10:00-12:00</li>
+					<li>活動場所：<a href="http://nbob.jp">NoblesseOblige</a></li>
+					<li>活動内容：ScratchやHTML/CSS/JavaScriptなどをつかったコンピュータプログラミングを子どもたちに教える</li>
+					<li>研修：有り。適宜実施致します。</li>
+				</ul>
+
+				<p class="text">【Kashiwa-no-ha】</p>
+				<ul>
+					<li>活動日時：毎月第3日曜日 10:00-12:00</li>
+					<li>活動場所：<a href="http://www.31ventures.jp/ventureoffice/koil/">KOIL</a></li>
+					<li>活動内容：CoderDojo Kashiwaの内容に同様</li>
+					<li>研修：有り。適宜実施致します。</li>
+				</ul>
+				<br><br>
+				<p>※応募用フォームはGogleフォームにリンクしています。</p>
+
+				<button class="btn"><a href="https://docs.google.com/forms/d/1VibGdKO6q8x7rvTS1Q0tA-50U4odzd_4UCfSfAQd5jA/viewform#responses" target="_blank" style="color: #fff;">応募する</a></button>
+
+		</div>
+
+
+	</div>
+		
 </div>
-
-		<?php
-
-			for ($i=0; $i < count($studentMentorData); $i+=1) { 
-				$imgNum = $i + 1;
-
-			print("<div id='modalStudent{$imgNum}' class='modal-content'>");
-
-				print("<h1>{$studentMentorData[$i]['name']}</h1>");
-
-				print("<h2>{$studentMentorData[$i]['positon']}</h2>");
-
-				print("<img src='image/mentorImage/s{$imgNum}.jpg'>");
-
-				print("<p>{$studentMentorData[$i]['comment']}</p>");
-
-				print("<a class='modal-close'>×</a>");
-	
-			print("</div>");
-
-			}
-
-			for ($i=0; $i < count($adultMentorData); $i+=1) { 
-				$imgNum = $i + 1;
-
-			print("<div id='modalAdult{$imgNum}' class='modal-content'>");
-
-				print("<h1>{$adultMentorData[$i]['name']}</h1>");
-
-				print("<h2>{$adultMentorData[$i]['positon']}</h2>");
-
-				print("<img src='image/mentorImage/a{$imgNum}.jpg'>");
-
-				print("<p>{$adultMentorData[$i]['comment']}</p>");
-
-				print("<a class='modal-close'>×</a>");
-	
-			print("</div>");
-
-			}
-
-		?>
-
-
 
 <?php
 	require_once 'footer.php';
