dd862098d39d1df075c44b8fd4ad84b12ed72a04
mjk0513 <kirie0513@gmail.com>
2017-08-04 21:04:06 +0900

    柏市プログラミング教育のページを追加

---
+++ project/kashiwaprogram/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="CoderDojo,CoderDojo Kashiwa,こーだーどーじょー,千葉,柏,プログラミング教室,小学生,中学生,プログラミング">
+	<link rel="stylesheet" type="text/css" href="../../style/style.css">
+    <link rel="stylesheet" type="text/css" href="./style/kpStyle.css">
+ 	<link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="./script/jquery.flexslider.js"></script>
+	
+	<title>柏市プログラミング教育</title>
+
+</head>
+
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+		<div id="menu">
+		<ul>
+			<li><a href="../../index.php"><img src="../../image/dojoLogo.png" class="homeLogo"></a></li>
+			<li><a href="../../about.php">CoderDojoとは</a></li>
+			<li><a href="../../dojo/index.php">近くのDojo</a></li>
+			<li><a href="../../join.php">参加する</a></li>
+			<li><a href="../../rtd.php">RTDとは</a></li>	
+			<li><a href="../../mentor.php">メンター</a></li>
+			<li><a href="../../event.php">イベント</a></li>
+		</ul>
+	</div>
---
+++ project/kashiwaprogram/index.php
@@ -0,0 +1,49 @@
+<?php
+    require_once "header.php"
+?>
+
+<div id="container">
+    <div id="kashiwaprogram">
+        <div id="titleHeader">
+           <img src="./image/header.png" class="kpHeaderImaegView">
+           <h1 class="kpHeaderTitleLabel">柏市とCoderDojo Kashiwaの取り組み</h1>
+           <p class="kpHeaderTextLabel">
+               柏市では2017年度より全国に先駆けてプログラミングを使った学習を市内にある42校すべての小学校4年生を対象にスタートさせました。
+               CoderDojo Kashiwaはカリキュラム開発・教職員研修等様々な場面で協力しています。<br>
+               ここでは、私たちの取り組みの一部をご紹介します。
+        </div>
+
+        <div id="kpMenu">
+            <ul>
+                <a href="#kpProject"><li>柏市の仕組み</li></a>
+                <a href="#kpCurriculum"><li>カリキュラム</li></a>
+                <a href="#kpEvent"><li>イベント</li></a>
+                <a href="#kpNews"><li>ニュース</li></a>
+            </ul>
+        </div>  
+
+        <div id="main">
+            <div id="kpProject">
+                <h1 class="kpTitle">柏市の取り組み</h1>
+            </div>
+
+            <div id="kpCurriculum">
+                <h1 class="kpTitle">カリキュラム</h1>
+            </div>
+
+            <div id="kpEvent">
+                <h1 class="kpTitle">イベント</h1>
+            </div>
+
+            <div id="kpNews">
+                <h1 class="kpTitle">ニュース</h1>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<?php
+    require_once "footer.php";
+    
